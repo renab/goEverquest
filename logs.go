837828c5205fd7ee930600c8164dde2861104b0e
Mortimus <Mortimus@gmail.com>
2021-03-13 14:35:14 -0600

    added log reading functions

---
+++ logs.go
@@ -0,0 +1,115 @@
+package everquest
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+const EQBaseLogLine = "\\[(\\w{3} \\w{3} \\d{2} \\d{2}:\\d{2}:\\d{2} \\d{4})] (.+)"
+
+type LogHandler interface {
+	ProcessLog(*EqLog)
+}
+
+func BufferedRead(path string, fromStart bool, pollRate int, handler LogHandler) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Printf("error opening buffered file: %v", err)
+		return
+	}
+	if !fromStart {
+		file.Seek(0, 2) // move to end of file
+	}
+	bufferedReader := bufio.NewReader(file)
+	r, _ := regexp.Compile(EQBaseLogLine)
+	for {
+		str, err := bufferedReader.ReadString('\n')
+		if err == io.EOF {
+			time.Sleep(time.Duration(pollRate) * time.Second) // 1 eq tick = 6 seconds
+			continue
+		}
+		if err != nil {
+			log.Printf("error opening buffered file: %v", err)
+			return
+		}
+
+		results := r.FindAllStringSubmatch(str, -1) // this really needs converted to single search
+		if results == nil {
+			time.Sleep(3 * time.Second)
+		} else {
+			log := readLogLine(results)
+			handler.ProcessLog(log)
+		}
+	}
+}
+
+func readLogLine(results [][]string) *EqLog {
+	t := eqTimeConv(results[0][1])
+	msg := strings.TrimSuffix(results[0][2], "\r")
+	log := &EqLog{
+		T:       t,
+		Msg:     msg,
+		Channel: getChannel(msg),
+		Source:  getSource(msg),
+	}
+	return log
+}
+
+func eqTimeConv(t string) time.Time {
+	// Get local time zone
+	localT := time.Now()
+	zone, _ := localT.Zone()
+
+	// Parse Time
+	cTime, err := time.Parse("Mon Jan 02 15:04:05 2006 MST", t+" "+zone)
+	if err != nil {
+		fmt.Printf("Error parsing time, defaulting to now: %s\n", err.Error())
+		cTime = time.Now()
+	}
+	return cTime
+}
+
+// EqLog represents a single line of eq logging
+type EqLog struct {
+	T       time.Time `json:"Time"`
+	Msg     string    `json:"Message"`
+	Channel string    `json:"Channel"`
+	Source  string    `json:"Source"`
+}
+
+func getChannel(msg string) string {
+	m := strings.Split(msg, " ")
+	if len(m) > 4 {
+		if m[3] == "guild," || m[4] == "guild," {
+			return "guild"
+		}
+		if m[3] == "group," || m[4] == "group," {
+			return "group"
+		}
+		if m[3] == "raid," || m[4] == "raid," {
+			return "raid"
+		}
+		if m[1] == "tells" && m[2] == "you," {
+			return "tell"
+		}
+		// fmt.Printf("Default: %s\n", m[2])
+		return "system"
+	}
+	if len(m) > 1 && m[1] == "tells" {
+		// return m[3]
+		return m[0]
+		// return strings.TrimRight(m[3], ",")
+	}
+	return "system"
+}
+
+func getSource(msg string) string {
+	m := strings.Split(msg, " ")
+	return m[0]
+}
