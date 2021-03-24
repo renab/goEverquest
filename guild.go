80028a89af86d9a27a374b46efae8b2b314d5c92
Mortimus <Mortimus@gmail.com>
2021-03-24 09:11:17 -0500

    added support for guild dumps

--- guild.go
+++ guild.go
@@ -1,9 +1,14 @@
 package everquest
 
 import (
+	"encoding/csv"
+	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func getRecentRosterDump(path string, guildName string) string {
@@ -19,4 +24,104 @@ func getRecentRosterDump(path string, guildName string) string {
 		panic(err)
 	}
 	return files[len(files)-1] // return last file - should be latest
+}
+
+type Guild struct {
+	Members []GuildMember
+}
+
+type GuildMember struct {
+	Name                string    // Character Name
+	Level               int       // Character Level
+	Class               string    // Player class ex: Necromancer
+	Rank                string    // Guild Rank Name
+	Alt                 bool      // Is this character flagged as an alt
+	LastOnline          time.Time // Last time this character was online
+	Zone                string    // Zone this character is currently in
+	PublicNote          string    // Public Note
+	PersonalNote        string    // Personal Note - assumed
+	TributeStatus       bool      // Tribute status on or off
+	TrophyTributeStatus bool      // Trophy Tribute Status on or off
+	Donations           int       // total donations
+	LastDonation        time.Time // Last date of donation
+	PublicNote2         string    // Seems to be the public note again not sure why
+	PersonalNote2       string    // Probably Personal Note again
+}
+
+// LoadFromPath takes a standard everquest raid dump and loads it into a struct
+func (guild *Guild) LoadFromPath(path string) {
+	// Open the file
+	tsvfile, err := os.Open(path)
+	if err != nil {
+		log.Fatalln("Couldn't open the tsv file", err)
+	}
+
+	// Parse the file
+	r := csv.NewReader(tsvfile)
+	r.Comma = '\t'
+	//r := csv.NewReader(bufio.NewReader(csvfile))
+
+	// Iterate through the records
+	for {
+		// Read each record from csv
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		level, err := strconv.Atoi(record[1])
+		if err != nil {
+			log.Printf("Error converting level to int - Level: %s Name: %s\n", record[1], record[0])
+			continue
+		}
+		var alt bool
+		if record[4] == "A" {
+			alt = true
+		}
+		format := "01/02/06"
+		lastOnline, err := time.Parse(format, record[5])
+		if err != nil {
+			log.Printf("Error converting last_online to time - Time: %s\n", record[5])
+			continue
+		}
+		var tributeStatus bool
+		if record[9] == "on" {
+			tributeStatus = true
+		}
+		var trophyTributeStatus bool
+		if record[10] == "on" {
+			trophyTributeStatus = true
+		}
+		donations, err := strconv.Atoi(record[11])
+		if err != nil {
+			log.Printf("Error converting donations to int - Donation: %s Name: %s\n", record[11], record[0])
+			continue
+		}
+		lastDonation, err := time.Parse(format, record[12])
+		if err != nil {
+			log.Printf("Error converting last_donation to time - Time: %s\n", record[12])
+			continue
+		}
+
+		guildMember := GuildMember{
+			Name:                record[0],
+			Level:               level,
+			Class:               record[2],
+			Rank:                record[3],
+			Alt:                 alt,
+			LastOnline:          lastOnline,
+			Zone:                record[6],
+			PublicNote:          record[7],
+			PersonalNote:        record[8],
+			TributeStatus:       tributeStatus,
+			TrophyTributeStatus: trophyTributeStatus,
+			Donations:           donations,
+			LastDonation:        lastDonation,
+			PublicNote2:         record[13],
+			PersonalNote2:       record[14],
+		}
+		guild.Members = append(guild.Members, guildMember)
+	}
 }
--- logs.go
+++ logs.go
@@ -103,8 +103,15 @@ func getChannel(msg string) string {
 	}
 	if len(m) > 1 && m[1] == "tells" {
 		// return m[3]
-		return m[0]
+		return "tell"
+		// return m[0] // source should show the player not the channel
 		// return strings.TrimRight(m[3], ",")
+	}
+	if len(m) > 1 && m[1] == "auctions" {
+		return "auction"
+	}
+	if len(m) > 1 && m[1] == "says" {
+		return "say"
 	}
 	return "system"
 }
